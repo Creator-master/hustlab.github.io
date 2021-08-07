5fef420ecbaafb90853151dd1dc5e958e8f93a8a
codebuddie <phillzou+github@gmail.com>
2021-08-07 09:12:09 +0800

    feat: add favo icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,20 +35,12 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favo-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-gatsby-cloud',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /assets/, // See below to configure properly
-        },
-      },
-    },
   ],
 };
